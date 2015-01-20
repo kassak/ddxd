d17fc66fb25bac1e9254455161672c8be3c05663
Alexander Kass <kass.soft@gmail.com>
2015-01-20 04:36:01 +0300

    init

---
+++ src/org/github/kassak/ddxd/TestAction.java
@@ -0,0 +1,13 @@
+package org.github.kassak.ddxd;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by kassak on 20.01.15.
+ */
+public class TestAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
